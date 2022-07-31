bb76c9b9079d5b0adfe008dff02a4c7c6116f682
mawuku <61219643+mawuku@users.noreply.github.com>
2022-08-01 01:12:16 +1000

    Update remove test class

--- src/test/java/io/github/mawuku/memberdashboard/memberdashboard/MemberdashboardApplicationTests.java
+++ src/test/java/io/github/mawuku/memberdashboard/memberdashboard/MemberdashboardApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.mawuku.memberdashboard.memberdashboard;
+/**package io.github.mawuku.memberdashboard.memberdashboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class MemberdashboardApplicationTests {
 	}
 
 }
+**/
